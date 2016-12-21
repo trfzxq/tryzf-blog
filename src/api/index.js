8de0fde7c026f38ceb6745c0939b06cacdb301ba
zhangfu-git <zhangfu@nasetech.com>
2016-12-21 17:31:29 +0800

    前端api添加获取导航栏，用户信息，社交信息接口

--- src/api/index.js
+++ src/api/index.js
@@ -1,13 +1,22 @@
-import { GetArticle, Login, GetArticleDetaile } from './resources'
+import * as types from './resources'
 
 export default {
   getArticle: function (limit, sizePage) {
-    return GetArticle.get({limit: limit, sizePage: sizePage})
+    return types.GetArticle.get({limit: limit, sizePage: sizePage})
   },
   getArticleDetaile: function (id) {
-    return GetArticleDetaile.get({id: id})
+    return types.GetArticleDetaile.get({id: id})
   },
   localLogin: function (data) {
-    return Login.save({id: 'local'}, data)
+    return types.Login.save({id: 'local'}, data)
+  },
+  getUserInfo: function () {
+    return types.GetUserInfo.get()
+  },
+  getNavList: function () {
+    return types.GetNavList.get()
+  },
+  getSocailContact: function () {
+    return types.GetSocailContact.get()
   }
 }
--- src/api/resources.js
+++ src/api/resources.js
@@ -8,3 +8,6 @@ Vue.http.options.credentials = true
 export const Login = Vue.resource('/api/login')
 export const GetArticle = Vue.resource('/api/getArticle?limit={limit}&sizePage={sizePage}')
 export const GetArticleDetaile = Vue.resource('/api/getArticleDetaile/{id}')
+export const GetUserInfo = Vue.resource('/api/getUserInfo')
+export const GetNavList = Vue.resource('/api/getNavList')
+export const GetSocailContact = Vue.resource('/api/getSocailContact')
