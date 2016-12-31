44c857e337b396a5c797b6590be236ce4527bfab
zhangfu-git <zhangfu@nasetech.com>
2016-12-31 17:42:21 +0800

    build

--- config/db_config.js
+++ config/db_config.js
@@ -2,7 +2,7 @@ let MONGODB_URI = 'mongodb://localhost/zhangfu'
 
 if (process.env.MONGODB_URI) {
   //MONGODBURL ：改为自己线上的mongodb_uri
-  MONGODB_URI = MONGODBURL
+  MONGODB_URI = 'mongodb://root:65oosDNoneacnzPVG73zCoevgXLFDFS0SHhTAvaF@clyqlklfouet.mongodb.sae.sina.com.cn:10523,ipzwyjblkjsv.mongodb.sae.sina.com.cn:10523'
 }
 
 module.exports = {
