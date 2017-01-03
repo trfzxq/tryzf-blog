ad36dce8b968241b757e3ca9ffb9d72fb4e6d50a
zhangfu-git <zhangfu@nasetech.com>
2017-01-03 23:08:35 +0800

    修改motto not default bug

--- src/store/actions.js
+++ src/store/actions.js
@@ -178,7 +178,7 @@ export default {
     })
   },
   updateUserInfo ({ commit }, data) {
-    if (!data.blogTitle || !data.headURL || !motto) {
+    if (!data.blogTitle || !data.headURL || !data.motto) {
       showModal(commit, {
         message: '添加些资料感觉好点吧！！！',
         type: 'info'
