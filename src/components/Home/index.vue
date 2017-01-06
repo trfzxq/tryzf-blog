2bca6aa1a6dad13959de2c0df480e40b30bec1b2
zhangfu-git <zhangfu@nasetech.com>
2017-01-07 00:18:25 +0800

    测试

--- src/components/Home/index.vue
+++ src/components/Home/index.vue
@@ -5,7 +5,7 @@
     <transition
       name="custom-classes-transition"
       enter-active-class="animated fadeIn"
-      leave-active-class="animated fadeOut"
+      leave-active-class="animated fadeInUp"
     >
       <router-view></router-view>
     </transition>
