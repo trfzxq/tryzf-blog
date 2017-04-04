fb394b3d288a28f6da894926ceeed8a1824519c6
zhangfu-git <zhangfu@nasetech.com>
2017-04-04 23:10:49 +0800

    update ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,12 +6,9 @@ module.exports = {
       name      : 'tryzf-blog',
       script    : 'server.js',
       env: {
-        COMMON_VARIABLE: 'true'
-      },
-      port: '3002',
-      env_production : {
         NODE_ENV: 'production'
-      }
-    },
+      },
+      port: '3002'
+    }
   ]
 };
